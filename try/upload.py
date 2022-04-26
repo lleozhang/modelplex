79f9d8da10dd8ec4983c11a13ddf00454996e55d
lleozhang <77916759+lleozhang@users.noreply.github.com>
2022-04-26 11:37:28 +0800

    Update upload.py

--- try/upload.py
+++ try/upload.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from Mod.models import ModInfo
 import os
 from . import s3
-
+#第二条测试
 def upload(request):
     ctx={}
     if not request.COOKIES.get('logged') or request.COOKIES.get('logged')!='true':
