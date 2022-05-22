5794d7195b6f5e6a69874277a850e3ad1725c90e
lleozhang <77916759+lleozhang@users.noreply.github.com>
2022-05-22 11:36:10 +0800

    Add files via upload

--- Datasetinfo/models.py
+++ Datasetinfo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class User(models.Model):
+class Dataset(models.Model):
     name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    owner = models.CharField(max_length=20)
+    modelid = models.DecimalField(max_digits=5, decimal_places=2)
+    visible=models.IntegerField()
+    accur = models.DecimalField(max_digits=5, decimal_places=2)
 # Create your models here.
