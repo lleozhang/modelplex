85f02fca4217347f0482d15cda60081e0fe3fecc
lleozhang <77916759+lleozhang@users.noreply.github.com>
2022-05-22 10:08:49 +0800

    Add files via upload

---
+++ try/Datasetinfo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    owner = models.CharField(max_length=20)
+    modelid = models.DecimalField(max_digits=5, decimal_places=2)
+    accur = models.DecimalField(max_digits=5, decimal_places=2)
+# Create your models here.
